45325f9958e37bef743001d05d042f101a20f1c0
albornet <alban.bornet@gmail.com>
2024-10-14 11:01:20 +0200

    added hpc support for patient csv files building

---
+++ data_utils.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Common sbatch variables
+PARTITION="shared-cpu"
+NUM_NODES=1
+NUM_TASKS=1
+TIME_LIMIT="0-00:30:00"
+TOTAL_CPU_MEMORY=64gb
+NUM_CPUS_PER_TASK=32
+
+# Script variables
+SIF_FOLDER=/home/users/b/borneta/sif
+SIF_NAME=mimic-triplets-image.sif
+SIF_IMAGE=${SIF_FOLDER}/${SIF_NAME}
+SCRIPT=data_utils.py
+
+# Build launch the script in apptainer container
+sbatch --job-name=data_utils \
+       --partition=$PARTITION \
+       --nodes=$NUM_NODES \
+       --ntasks=$NUM_TASKS \
+       --cpus-per-task=$NUM_CPUS_PER_TASK \
+       --mem=$TOTAL_CPU_MEMORY \
+       --time=$TIME_LIMIT \
+       --output=./results/logs/job_%j.txt \
+       --error=./results/logs/job_%j.err \
+       --wrap="srun apptainer exec ${SIF_IMAGE} python ${SCRIPT} --runtype=${RUNTYPE}"
---
+++ mimic-triplets-image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=singularity_build
+#SBATCH --partition=shared-gpu,private-teodoro-gpu
+#SBATCH --nodelist=gpu023,gpu024,gpu036,gpu037,gpu038,gpu039,gpu040,gpu041,gpu042,gpu043
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --time=00:30:00
+#SBATCH --output=./logs/singularity_build_%j.txt
+#SBATCH --error=./logs/singularity_build_%j.err
+
+# Check if the .sif file exists, and only remove it if it does
+if [ -f ~/sif/mimic-triplets-image.sif ]; then
+    rm ~/sif/mimic-triplets-image.sif
+fi
+
+# Build the Singularity image
+apptainer build ~/sif/mimic-triplets-image.sif mimic-triplets-image.def
